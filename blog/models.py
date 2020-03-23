f190e25412ed7175296e177f1131d9b6202ae7c4
cz8123 <691120770@qq.com>
2020-03-23 20:34:52 +0800

    20200323

--- blog/models.py
+++ blog/models.py
@@ -4,9 +4,13 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=70)
@@ -17,5 +21,7 @@ class Post(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tag = models.ManyToManyField(Tag, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
 
     
