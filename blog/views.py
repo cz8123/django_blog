aab35d3417fce3da3bef137dafdf3f83c9747a0a
cz8123 <691120770@qq.com>
2020-04-21 11:11:36 +0800

    20200421

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 # from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    # return HttpResponse('欢迎访问我的博客首页！')
+    post_list = Post.objects.all().order_by('-created_time')
     return render(request, 'blog/index.html', context={
-        'title': '我的博客首页',
-        'welcome': '欢迎访问我的博客首页',
+        'post_list': post_list,
+        
     })
 
 
